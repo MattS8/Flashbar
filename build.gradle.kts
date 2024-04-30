764239feab52610b11833d819fd577e18b904477
Matthew Steinhardt <Matthew.steinhardt@gmail.com>
2024-04-30 00:10:51 -0700

    Apply plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,7 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     alias(libs.plugins.androidLibrary) apply false
-    id("maven-publish") apply true
+}
+apply {
+    plugin("maven-publish")
 }
